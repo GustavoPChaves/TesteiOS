81d4516bed9c215f82c5f26e1a47cd684a686305
Gustavo Chaves <gustavoportelada@gmail.com>
2020-04-23 11:32:02 -0300

    Criação da estrutura de pastas

---
+++ TesteiOS/TesteiOS/Services/EndPoints.swift
@@ -0,0 +1,19 @@
+//
+//  EndPoint.swift
+//  TesteiOS
+//
+//  Created by Gustavo Chaves on 22/04/20.
+//  Copyright © 2020 Gustavo Chaves. All rights reserved.
+//
+
+import Foundation
+
+enum EndPoints {
+    static var form: String {
+        return "https://floating-mountain-50292.herokuapp.com/cells.json"
+    }
+    static var screen: String {
+        return "https://floating-mountain-50292.herokuapp.com/fund.json"
+    }
+}
+
